beb91d8d23f16bde1a0cf23358145e49f90697df
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-04-11 23:10:41 +0700

    make domain layer

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/xvbnm48/golang-hexagonal/internal/adapters/core/arithmetic"
+	"github.com/xvbnm48/golang-hexagonal/internal/ports"
+)
+
+func main() {
+
+	// ports
+	var core ports.ArithmeticPort
+
+	core = arithmetic.NewAdapter()
+
+	arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 3)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
